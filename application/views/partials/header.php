119d36f031c6c4c7bd7010ade8fa5e167e124110
Chinedu Rowland <chinicerow@yahoo.com>
2017-08-03 13:54:20 +0100

    Updates to notifcation bar

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -96,23 +96,6 @@
 
                 <!-- Right(Notification and Searchbox -->
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <!-- Notification -->
-                        <div class="notification-box">
-                            <ul class="list-inline m-b-0">
-                                <li>
-                                    <a href="javascript:void(0);" class="right-bar-toggle">
-                                        <i class="zmdi zmdi-notifications-none"></i>
-                                    </a>
-                                    <div class="noti-dot">
-                                        <span class="dot"></span>
-                                        <span class="pulse"></span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- End Notification bar -->
-                    </li>
                     <li class="hidden-xs">
                         <form role="search" class="app-search">
                             <input type="text" placeholder="Search..."
